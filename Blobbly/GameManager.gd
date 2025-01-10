6fe86f66a72291fbeb7beea4f5b74505201deb44
Rozzy1 <rossameehan@gmail.com>
2025-01-10 19:05:38 +0000

    Added more stuff

--- Blobbly/GameManager.gd
+++ Blobbly/GameManager.gd
@@ -1,5 +1,25 @@
 extends Node
-var Player1 = Player
-
+var Player1 = Player.new()
+var testarray : Array[InputModel]
 func _process(_delta: float) -> void:
-	pass
+	Write_Inputs()
+	
+	
+	
+
+func Read_Inputs():
+	for i in testarray.size():
+		var testinputs = testarray[i]
+		Player1.leftright = testinputs.left - testinputs.right
+	
+
+
+func Write_Inputs():
+	var inputs = InputModel.new()
+	inputs.dash = Input.get_action_strength("Dash")
+	inputs.up = Input.get_action_strength("up")
+	inputs.left = Input.get_action_strength("left")
+	inputs.right = Input.get_action_strength("right")
+	if inputs.up or inputs.right or inputs.left or inputs.dash:
+		testarray.append(inputs)
+		print(testarray)
---
+++ Blobbly/InputModel.gd
@@ -0,0 +1,13 @@
+
+class_name InputModel
+var up : int
+var right : int
+var left : int
+var dash : int
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
